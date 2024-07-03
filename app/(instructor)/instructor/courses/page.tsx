982c2926ba8a08bcd459bae1dcbad71361c23add
sandy <sandeshjadhav2002@gmail.com>
2024-07-03 19:57:58 +0530

    Sidebar

---
+++ app/(instructor)/instructor/courses/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+const CoursesPage = () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  return <div className="px-6 py-4">
+    <Link href="/instructor/create-course"> <Button>Create New course</Button> </Link>
+  </div>;
+};
+
+export default CoursesPage;
--- components/layout/Topbar.tsx
+++ components/layout/Topbar.tsx
@@ -4,7 +4,7 @@ import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 
 const Topbar = () => {
     const { isSignedIn } = useAuth()
