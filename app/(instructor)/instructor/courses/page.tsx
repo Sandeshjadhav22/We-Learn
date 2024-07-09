d67933016dd1a6a9b896b1fc8a47d9e3605f57a0
sandy <sandeshjadhav2002@gmail.com>
2024-07-09 14:46:54 +0530

    course table

--- app/(instructor)/instructor/courses/page.tsx
+++ app/(instructor)/instructor/courses/page.tsx
@@ -4,30 +4,36 @@ import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import { DataTable } from "@/components/custom/DataTable";
+import { columns } from "@/components/courses/Columns";
 
-const CoursesPage = async() => {
+const CoursesPage = async () => {
   const { userId } = auth();
+
   if (!userId) {
     return redirect("/sign-in");
   }
 
   const courses = await db.course.findMany({
-     where: {
-      instructorId: userId
-     },
-     orderBy:{
-      createdAt: 'desc'
-     }
-  })
+    where: {
+      instructorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-  return <div className="px-6 py-4">
-    <Link href="/instructor/create-course"> <Button>Create New course</Button> </Link>  
-    <div className="mt-10">
-      {courses.map((course) =>  (
-        <Link href={`/instructor/courses/${course.id}/basic`} key={course.id}>{course.title}</Link>
-      ))}
+  return (
+    <div className="px-6 py-4">
+      <Link href="/instructor/create-course">
+        <Button>Create New Course</Button>
+      </Link>
+
+      <div className="mt-5">
+        <DataTable columns={columns} data={courses} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CoursesPage;
